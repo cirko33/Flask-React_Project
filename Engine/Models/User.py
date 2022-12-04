f3752138dc5ed7be8ccee19dabd78f364696b1b9
KurdulijaD <dejankurdulija00@gmail.com>
2022-12-04 15:54:17 +0100

    Extending the user model for the verified field

--- Engine/Models/User.py
+++ Engine/Models/User.py
@@ -1,5 +1,6 @@
 from Engine.Configuration.config import db
 from marshmallow import Schema, fields
+
 class User(db.Model):
     __tablename__ = 'user'
     email = db.Column(db.String(64), primary_key=True)
@@ -9,8 +10,9 @@ class User(db.Model):
     address = db.Column(db.String(64))
     city = db.Column(db.String(32))
     phoneNumber = db.Column(db.Integer)
+    verified = db.Column(db.Boolean, default=False)
 
-    def __init__(self, firstName, lastName, address, city, phoneNumber, email, password):
+    def __init__(self, firstName, lastName, address, city, phoneNumber, email, password, verified):
         self.firstName = firstName
         self.lastName = lastName
         self.address = address
@@ -18,6 +20,7 @@ class User(db.Model):
         self.phoneNumber = phoneNumber
         self.email = email
         self.password = password
+        self.verified = verified
 
 class UserSchema(Schema):
     email = fields.Str()
@@ -27,3 +30,5 @@ class UserSchema(Schema):
     address = fields.Str()
     city = fields.Str()
     phoneNumber = fields.Number()
+    verified = fields.Boolean()
+
