3f581bc2e154e5588aabdd1dc74deed483815be9
MilinkovicZ <zdravkolegenda123@gmail.com>
2022-12-06 14:10:33 +0100

    Implementing credit card methods

--- Engine/Models/CreditCard.py
+++ Engine/Models/CreditCard.py
@@ -5,19 +5,22 @@ from datetime import datetime
 class CreditCard(db.Model):
     __tablename__ = 'credit_card'
     cardNumber = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32))
+    userName = db.Column(db.String(32))
     expirationDate = db.Column(db.DateTime, default = datetime.utcnow)
     cvc = db.Column(db.Integer)
+    userEmail = db.Column(db.String(64))
 
 
-    def __init__(self, cardNumber, name, expirationDate, cvc):
+    def __init__(self, cardNumber, userName, expirationDate, cvc, userEmail):
         self.cardNumber = cardNumber
-        self.name = name
+        self.userName = userName
         self.expirationDate = expirationDate
         self.cvc = cvc
+        self.userEmail = userEmail
 
 class CreditCardSchema(Schema):
     cardNumber = fields.Number()
     name = fields.Str()
     expirationDate = fields.Str()
     cvc = fields.Number()
+    userEmail = fields.Str()
--- Engine/Routes/Card.py
+++ Engine/Routes/Card.py
@@ -1,23 +1,71 @@
-from Configuration.config import api
+from Configuration.config import *
 from flask_restful import Resource
+
+creditCardAddingArgs = reqparse.RequestParser()
+creditCardAddingArgs.add_argument("cardNumber", type=int, help="Card Number is required", required=True)
+creditCardAddingArgs.add_argument("userName", type=str, help="user name is required", required=True)
+creditCardAddingArgs.add_argument("expirationDate", type=str, help="Date is required", required=True)
+creditCardAddingArgs.add_argument("cvc", type=int, help="CVC is required", required=True)
+creditCardAddingArgs.add_argument("userEmail", type=str, help="user email is required", required=True)
+creditCardAddingArgs.add_argument("amount", type=float, help="amount is required", required=True)
 
 #Credit card get and post (adding credit card)
 class Card(Resource):
-    def get(self):
-        return "card-get"
-        
+    def get(self, data):
+        if isinstance(data, int):
+            temp = Card.query.filter_by(cardNumber=data).first();
+            if not temp:
+                return "Card with this number doesn't exist", 404
+            else:
+                return jsonify(temp), 200
+        else:
+            temp = Card.query.filter_by(userEmail=data).first()
+            if not temp:
+                return "User with this email does not own any cards", 404
+            else:
+                return jsonify(temp), 200
+    
+    #za racun vrv jer ne pravimo karticu
     def post(self):
-        return "card-post"
+        args = creditCardAddingArgs.parse_args()
+        
+        try:
+            temp = Card.query.filter_by(cardNumber=args['cardNumber']).first()
+            if temp:
+                return "Card with this number already exists!", 404
+        except:
+            return "Server failed", 500
+
+        card = Card(cardNumber=args['cardNumber'], userName=args['userName'], 
+        expirationDate=args['expirationDate'], cvc=args['cvc'], userEmail=args['userEmail'], amount=args["amount"])        
+        db.session.add(card)
+        db.session.commit()
+        return jsonify(card), 200
 
 api.add_resource(Card, "/card")
 
 #Credit card balance get and post (withdraw of money)
 class CardBalance(Resource):
-    def get(self):
-        return "card-balance-get"
+    def get(self, card_number):
+        temp = Card.query.filter_by(cardNumber=card_number).first();
+        if not temp:
+            return "Card with this number doesn't exist", 404
+        else:
+            return temp.amount, 200
     
-    def post(self):
-        return "card-balance-post"
+    def post(self, card_number, value, type):
+        temp = Card.query.filter_by(cardNumber=card_number).first();
+        if (type == 1): #deposit
+            temp.amount += value
+        elif (type == 2): #withdraw
+            if value > temp.amount:
+                return "You dont have enought money!", 404
+            else:
+                temp.amount -= value 
+        else:
+            return "Error!", 404
+        
+        db.session.commit();
+        return 200;
 
 api.add_resource(CardBalance, "/balance")
-
