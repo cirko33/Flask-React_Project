b720df318fa5e7749c50383a8c20c71f1d7e31c9
KurdulijaD <dejankurdulija00@gmail.com>
2022-12-07 22:26:15 +0100

    Operation for history of transactions for user

--- Engine/Routes/Transaction.py
+++ Engine/Routes/Transaction.py
@@ -1,10 +1,18 @@
-from Configuration.config import api
+from Configuration.config import api, jsonify
 from flask_restful import Resource
+from Models import *
 
 #Transaction get (history) and post (making new transaction)
 class Transaction(Resource):
-    def get(self):
-        return "transaction-get"
+    def get(self, userEmail):
+        transactionSender = Transaction.query.filter_by(sender = userEmail).all()
+        transactionReceiver = Transaction.query.filter_by(receiver = userEmail).all()
+        transaction = transactionSender + transactionReceiver
+        
+        if transaction:
+            return jsonify(transaction), 200
+        else:
+            return "You don't have any transaction.", 400
     
     def post(self):
         return "transaction-post"
