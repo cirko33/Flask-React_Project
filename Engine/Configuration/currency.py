704eb37bc2e083a33f3523d78be460758086a768
cirko33 <luka.ciric2106@gmail.com>
2022-12-30 17:09:14 +0100

    Fixing currency

--- Engine/Configuration/currency.py
+++ Engine/Configuration/currency.py
@@ -1,35 +1,39 @@
 #gets currency rates from API once a day
 import json, sys, datetime, urllib.request
 
+path = "./Configuration/rates.json"
 def getRatesFromAPI():
     url = 'http://api.exchangeratesapi.io/v1/latest?access_key=57d102e76d357aaaab5dba8955ffa5a8'
     data = urllib.request.urlopen(url).read()
     return json.loads(data)["rates"]
 
-def getDataToSave(file):
-    date = datetime.datetime.now().strftime("%d/%m/%Y")
-    rates = getRatesFromAPI()
-    file.write(json.dumps({date:date, rates:rates}))
+def getDataToSave():
+    with open(path, "w+") as file:
+        date = datetime.datetime.now().strftime("%d/%m/%Y")
+        rates = getRatesFromAPI()
+        file.write(json.dumps({"date":date, "rates":rates}))
     return rates
 
 def getRates():
-    with open("rates.json", "w+") as file:
-        try:
-            text = file.read()
-            if(text == ""):
-                return getDataToSave(file)
+    text = ""
+    with open(path, "w+") as file:
+        text = file.read()
 
-            data = json.loads(text)
-            if(["date", "rates"] not in data.keys()):
-                return getDataToSave(file)
+    try:
+        if(text == ""):
+            return getDataToSave()
 
-            if(data["date"] != datetime.datetime.now().strftime("%d/%m/%Y")):
-                return getDataToSave(file)
-            
-            return data["rates"]
-        except Exception as e:
-            print(str(e), sys.stderr)
-            return getDataToSave(file)
+        data = json.loads(text)
+        if(["date", "rates"] not in data.keys()):
+            return getDataToSave()
+
+        if(data["date"] != datetime.datetime.now().strftime("%d/%m/%Y")):
+            return getDataToSave()
+        
+        return data["rates"]
+    except Exception as e:
+        print(str(e), sys.stderr)
+        return getDataToSave(file)
 
 def exchangeMoney(value, curr1, curr2):
     rates = getRates()
@@ -40,9 +44,9 @@ def exchangeMoney(value, curr1, curr2):
         raise Exception(curr2 + " doesn't exist in our system.")
 
     if(curr1 == "EUR"):
-        return value * float(rates["curr2"])
+        return value * float(rates[curr2])
     
     if(curr2 == "EUR"):
-        return value / float(rates["curr1"])
+        return value / float(rates[curr1])
 
-    return value / float(rates["curr1"]) * float(curr2)
+    return value / float(rates[curr1]) * float(rates[curr2])
