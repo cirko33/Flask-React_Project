4e178931acbdd5d8c6254949eb2c72f350537243
KurdulijaD <dejankurdulija00@gmail.com>
2022-12-31 17:48:36 +0100

    Some testing

--- UI/src/components/Transactions/Transactions.js
+++ UI/src/components/Transactions/Transactions.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect} from "react";
-import styles from './Transactions.module.css'
+import React, { useState, useEffect } from "react";
+import "./Transactions.module.css";
 
 const Transactions = () => {
   const [transactions, setTransactions] = useState([]);
@@ -8,7 +8,7 @@ const Transactions = () => {
 
   const getTransactions = async () => {
     const response = await fetch(
-      "http://localhost:5000/transaction/" + sessionStorage.getItem('user')
+      "http://localhost:5000/transaction/" + sessionStorage.getItem("user")
     );
 
     if (!response.ok) {
@@ -18,33 +18,31 @@ const Transactions = () => {
     const data = await response.json();
     const loadedTransactions = [];
     for (const key in data) {
-        loadedTransactions.push({
+      loadedTransactions.push({
         id: key,
         pk: data[key].pk,
         currency: data[key].currency,
         amount: data[key].amount,
         state: data[key].state,
-        sender: data[key].sender
+        sender: data[key].sender,
       });
     }
 
     setTransactions(loadedTransactions);
-    console.log(loadedTransactions);
-  
   };
 
   useEffect(() => {
-    getTransactions().catch((error) => {      
+    getTransactions().catch((error) => {
       console.log(error.message);
     });
   }, []);
 
   const handleSortClick = (field) => {
     if (sortField === field) {
-      setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
     } else {
       setSortField(field);
-      setSortDirection('asc');
+      setSortDirection("asc");
     }
   };
 
@@ -52,64 +50,60 @@ const Transactions = () => {
   if (sortField) {
     sortedTransactions.sort((a, b) => {
       if (a[sortField] < b[sortField]) {
-        return sortDirection === 'asc' ? -1 : 1;
+        return sortDirection === "asc" ? -1 : 1;
       }
       if (a[sortField] > b[sortField]) {
-        return sortDirection === 'asc' ? 1 : -1;
+        return sortDirection === "asc" ? 1 : -1;
       }
       return 0;
     });
   }
-      
 
-  let content = (
+  return (
     <React.Fragment>
-    <h1>YOUR TRANSACTIONS:</h1>
-    <table>
-  <thead>
-    <tr>
-      <th onClick={() => handleSortClick('sender')}>
-        Sender
-        {sortField === 'sender' && (
-          <span>{sortDirection === 'asc' ? '▲' : '▼'}</span>
-        )}
-      </th>
-      <th onClick={() => handleSortClick('amount')}>
-        Amount
-        {sortField === 'amount' && (
-          <span>{sortDirection === 'asc' ? '▲' : '▼'}</span>
-        )}
-      </th>
-      <th onClick={() => handleSortClick('currency')}>
-        Currency
-        {sortField === 'currency' && (
-          <span>{sortDirection === 'asc' ? '▲' : '▼'}</span>
-        )}
-      </th>
-      <th onClick={() => handleSortClick('state')}>
-        State
-        {sortField === 'state' && (
-          <span>{sortDirection === 'asc' ? '▲' : '▼'}</span>
-        )}
-      </th>
-    </tr>
-  </thead>
-  <tbody>
-    {sortedTransactions.map((transaction) => (
-      <tr>
-        <td>{transaction.sender}</td>
-        <td>{transaction.amount}</td>
-        <td>{transaction.currency}</td>
-        <td>{transaction.state}</td>
-      </tr>
-    ))}
-  </tbody>
-</table>
-
+      <h1>YOUR TRANSACTIONS:</h1>
+      <table className="transaction-table">
+        <thead>
+          <tr>
+            <th onClick={() => handleSortClick("sender")}>
+              Sender
+              {sortField === "sender" && (
+                <span>{sortDirection === "asc" ? "▲" : "▼"}</span>
+              )}
+            </th>
+            <th onClick={() => handleSortClick("amount")}>
+              Amount
+              {sortField === "amount" && (
+                <span>{sortDirection === "asc" ? "▲" : "▼"}</span>
+              )}
+            </th>
+            <th onClick={() => handleSortClick("currency")}>
+              Currency
+              {sortField === "currency" && (
+                <span>{sortDirection === "asc" ? "▲" : "▼"}</span>
+              )}
+            </th>
+            <th onClick={() => handleSortClick("state")}>
+              State
+              {sortField === "state" && (
+                <span>{sortDirection === "asc" ? "▲" : "▼"}</span>
+              )}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedTransactions.map((transaction) => (
+            <tr>
+              <td className="sender">{transaction.sender}</td>
+              <td className="amount">{transaction.amount}</td>
+              <td className="currency">{transaction.currency}</td>
+              <td className="state">{transaction.state}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </React.Fragment>
   );
-
-  return <div className={styles.transactions}>{content}</div>
 };
 
 export default Transactions;
