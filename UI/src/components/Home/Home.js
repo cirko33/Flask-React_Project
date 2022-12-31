f2357ddc611f87aae500b635bd0c92e4be6a3ea8
KurdulijaD <dejankurdulija00@gmail.com>
2022-12-31 17:12:49 +0100

    Suggestion for css, needs more work

--- UI/src/components/Home/Home.js
+++ UI/src/components/Home/Home.js
@@ -4,15 +4,18 @@ import Balances from '../Balances/Balances';
 import Exchange from '../Exchange/Exchange';
 import Transactions from '../Transactions/Transactions';
 import Button from '../common/Button';
+import styles from './Home.module.css'
 
 const Home = (props) => {
     const [mode, setMode] = useState(false); //if true exchange money, false transactions
     return (<React.Fragment>
         {!props.verified && <Verify setVerified={props.setVerified}/>}
-        {props.verified && <Balances />}
         {props.verified && mode && <Exchange />}
-        {props.verified && !mode && <Transactions />} 
         {props.verified && <Button onClick={(event) => {setMode(!mode)}}>{mode ? "View transactions" : "Exchange money"}</Button>}
+        <div className={styles.parent}>
+            <div className={styles.leftContent}>{props.verified && !mode && <Transactions />} </div>
+            <div className={styles.rightContent}>{props.verified && <Balances />}</div>
+        </div>
     </React.Fragment>)
 };
 
