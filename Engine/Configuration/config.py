e90c04c9451916dfe34c7c4b265fb012e224938a
cirko33 <luka.ciric2106@gmail.com>
2023-01-18 14:47:50 +0100

    Fixing socket to websocket

--- Engine/Configuration/config.py
+++ Engine/Configuration/config.py
@@ -5,6 +5,7 @@ from flask_marshmallow import Marshmallow
 from flaskext.mysql import MySQL
 from flask_cors import CORS
 import socket, hashlib
+from flask_socketio import SocketIO, emit, join_room, request
 
 app = Flask(__name__)
 
@@ -23,6 +24,7 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 mysql = MySQL(app)
 CORS(app)
+socketio = SocketIO(app)
 
 activeTokens = { } #Has tokens as keys and email addresses as values
 
@@ -31,3 +33,8 @@ sendingSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 def createHash(text, end="_qw3efdsfg1"):
     textToHash = text + end
     return hashlib.sha256(textToHash.encode("utf8")).hexdigest()
+
+@socketio.on('connect')
+def connect_socket():
+    join_room(request.sid)
+    
